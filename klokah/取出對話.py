763891a651de09418602d217225f8c1f98dcd265
薛丞宏 <ihcaoe@gmail.com>
2015-04-30 16:20:26 +0800

    基本對話 丞宏

---
+++ klokah/取出對話.py
@@ -0,0 +1,46 @@
+from xml.etree import ElementTree
+
+class 文件檔資料擷取:
+	def 列出對話資料(self, 檔案位置):
+		樹 = ElementTree.parse(檔案位置)
+		根 = 樹.getroot()
+		for item in 根:
+			# 檢查type
+			資料編號 = self._找出元素資料(item, 'sentenceOrder')
+			資料 = []
+			
+			族語資料 = [
+				self._找出元素資料(item, 'sentenceAAb'),
+				self._找出元素資料(item, 'sentenceBAb'),
+				self._找出元素資料(item, 'sentenceCAb'),
+				]
+			華語資料 = [
+				self._找出元素資料(item, 'sentenceACh'),
+				self._找出元素資料(item, 'sentenceBCh'),
+				self._找出元素資料(item, 'sentenceCCh'),
+				]
+			for 族語, 華語 in zip(族語資料, 華語資料):
+				if 族語:
+					資料.append((族語, 華語))
+			yield 資料編號, 資料
+	def _找出元素資料(self, 物件, 底下tag):
+		結果 = 'None'
+		for 編號, 資料 in enumerate(物件.iter(底下tag)):
+			結果 = 資料.text
+			if 編號 > 0:
+				raise RuntimeError('資料超過一個！！')
+		if 結果 == 'None':
+			raise RuntimeError('沒此子資料！！')
+		return 結果
+
+if __name__ == '__main__':
+	檔案位置 = '/home/tshau/下載/南排/26原住民學生補充教材離線系統（高中版）/senior/xml/3/16.xml'
+	對話資料 = 文件檔資料擷取().列出對話資料(檔案位置)
+	for 資料編號, 資料 in 對話資料:
+		print(資料編號)
+		for 族語, _ in 資料:
+			print(族語)
+		print()
+		for _, 華語 in 資料:
+			print(華語)
+		print()
